32295719414a1b62228d6fb235255956cfc6a68f
smokejel <65026298+smokejel@users.noreply.github.com>
2025-12-30 23:20:50 -0500

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({
@@ -34,6 +35,7 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
